6f66a69cd9197c03eee01263fbffa7ae7c9fd650
Webert Maximiano <webertcoach@gmail.com>
2023-08-11 15:09:49 -0300

    exercicio concluido

--- src/App.js
+++ src/App.js
@@ -9,23 +9,41 @@ import Budget from './components/Budget';
 
 
 import { AppProvider } from './context/AppContext';
+import Remaining from './components/Remaining';
+import ExpenseTotal from './components/ExpenseTotal';
+import ExpenseItem from './components/ExpenseItem';
+import ExpenseList from './components/ExpenseList';
+import AllocationForm from './components/AllocationForm';
+
 const App = () => {
     return (
         <AppProvider>
             <div className='container'>
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
                     <div className='row mt-3'>
-                        {/* Add Budget component here under */}        
+                        <div className='col-sm'>
+                        <Budget />
+                     </div>   
 
-                        {/* Add Remaining component here under */}        
+                     <div className='col-sm'>
+                        <Remaining />
+                    </div>       
 
-                        {/* Add ExpenseTotal component here under */}        
-                       
-                        {/* Add ExpenseList component here under */}         
+                    <div className='col-sm'>
+                        <ExpenseTotal />
+                    </div>
+                    
+                    <div className='col-sm'>
+                        <ExpenseList />
+                    </div>
 
-                        {/* Add ExpenseItem component here under */}        
+                    <div className='col-sm'>
+                        <ExpenseItem />
+                    </div>       
 
-                        {/* Add AllocationForm component here under */}        
+                    <div className='col-sm'>
+                        <AllocationForm/>
+                    </div>          
 
                 </div>
             </div>
